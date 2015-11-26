86e234b6159da3ace1b56ffdd2059c13f816ddd9
Matias Carrasco Kind <mgckind@gmail.com>
2015-11-26 02:08:43 -0600

    update __init__.py

--- python/lsst/pipe/base/__init__.py
+++ python/lsst/pipe/base/__init__.py
@@ -3,6 +3,7 @@ from .argumentParser import *
 from .struct import *
 from .cmdLineTask import *
 from .timer import *
-#from .activator import *
+from .activator import *
+from .workflow import *
 from .super_task import *
 from .task import *
